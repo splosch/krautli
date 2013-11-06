49ee2dde94394912873fd02189a15b6d48bf6094
splosch <sploscho@googlemail.com>
2013-11-06 10:49:00 +0100

    Use factory for suggestion-list creation

--- app/scripts/controllers/suggestNameController.js
+++ app/scripts/controllers/suggestNameController.js
@@ -1,17 +1,34 @@
 'use strict';
 
 angular.module('krautli_yoApp')
-  .controller('SuggestNameController', function ($scope) {
+  .factory('plantListFactory', function () {
+  	var factory = {},
+  		initialPlantList = [
+	      { name: 'Ackergauchheil' },
+	      { name: 'Arnika' },
+	      { name: 'Beifuss' },
+	      { name: 'Eberesche' }
+	    ];
+
+	factory.getPlantList = function () {
+		return initialPlantList;
+	};    
+
+	factory.addPlant = function ( plant ) {
+		initialPlantList.push( plant );
+		return initialPlantList;
+	};  
+
+  	return factory;
+  })
+  .controller('SuggestNameController', function ($scope, plantListFactory) {
 
   	// still mocked - TODO:[LOW] replace with initial data
-    $scope.plantNames = [
-      { name: 'Ackergauchheil' },
-      { name: 'Arnika' },
-      { name: 'Beifuss' },
-      { name: 'Eberesche' }
-    ];
+    $scope.plantNames = plantListFactory.getPlantList();
 
     $scope.addNewPlantName = function () {
-    	$scope.plantNames.push({name: $scope.searchterm});
+    	var newPlant = { name: $scope.searchterm };
+    	
+    	plantListFactory.addPlant( newPlant );
     }
   });
