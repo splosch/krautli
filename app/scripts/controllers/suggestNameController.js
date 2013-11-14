c7d5a499f00586cf55becb2bcef8b84049ffdb17
splosch <sploscho@googlemail.com>
2013-11-14 18:10:38 +0100

    Condition to add new P]plants only

--- app/scripts/controllers/suggestNameController.js
+++ app/scripts/controllers/suggestNameController.js
@@ -27,8 +27,22 @@ angular.module('krautli_yoApp')
     $scope.plantNames = plantListFactory.getPlantList();
 
     $scope.addNewPlantName = function () {
-    	var newPlant = { name: $scope.searchterm };
-    	
+    	var newPlant = {};
+
+    	debugger;
+    	if ( !$scope.searchterm  || typeof $scope.searchterm !== "string" || $scope.searchterm.length < 4 ) {
+    		return false;
+    	}
+
+    	newPlant.name = $scope.searchterm;
+
     	plantListFactory.addPlant( newPlant );
-    }
+    };
+
+    $scope.showAddNewPlantButton = function () {
+    	var hasResults = false;
+
+    	return !hasResults;
+    };
+
   });
